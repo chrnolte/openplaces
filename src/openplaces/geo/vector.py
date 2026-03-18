4a448c149dc4b0e2f472c82c13b70275a7054a39
Christoph Nolte <nolte.christoph@gmail.com>
2026-03-18 17:47:46 -0400

    Added creation of tile-admin links to ingest_tiles.ipynb and vector.py

--- src/openplaces/geo/vector.py
+++ src/openplaces/geo/vector.py
@@ -26,7 +26,8 @@ from openplaces.core.constants import (
     M2_TO_SQFT,
     STRING_SEPARATOR_BETWEEN_IDS,
 )
-from openplaces.recipe import get_recipe
+from openplaces.io import save_parquet
+from openplaces.recipe import get_output_path, get_recipe
 from openplaces.timing import get_timer
 
 PROJ4 = {
@@ -998,3 +999,41 @@ def overlay_polygons(
         return gpd.GeoDataFrame(overlaps, geometry='geometry', crs=crs1)
 
     return overlaps
+
+
+def create_entity_link(entity1_recipe_id, entity2_recipe_id, save=True, **kwargs):
+    """Create ID linkage (n-to-m) between two entities and save
+
+    Parameters
+    ----------
+    entity1_recipe_id : str
+        Recipe ID of first entity
+    entity2_recipe_id : str
+        Recipe ID of second entity
+    save : bool
+        If True, save output as a file, in the folder of `entity1`:
+        {entity1_recipe_id}_{entity2_recipe_id}.parquet
+    kwargs : dict
+        Are passed to `openplaces.geo.vector.overlay_polygons`
+    """
+
+    entity1_path = get_output_path(entity1_recipe_id)
+    entity2_path = get_output_path(entity2_recipe_id)
+
+    if not entity1_path.exists():
+        print(f'Link creation aborted. Data not found: {entity1_path}')
+        return
+
+    if not entity2_path.exists():
+        print(f'Link creation aborted. Data not found: {entity2_path}')
+        return
+
+    entity1_entity2_link = overlay_polygons(entity1_path, entity2_path, **kwargs)
+
+    if save:
+        entity1_entity2_link_path = entity1_path.with_name(
+            entity1_path.stem + f'_{entity2_recipe_id}.parquet'
+        )
+        save_parquet(entity1_entity2_link, entity1_entity2_link_path)
+
+    return entity1_entity2_link
