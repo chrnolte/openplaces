0f4d1db3eb92367a364f2b97699867c871706357
Christoph Nolte <nolte.christoph@gmail.com>
2026-06-09 15:23:16 -0400

    test: update path expectations for _all escape directory

--- tests/test_path.py
+++ tests/test_path.py
@@ -1,22 +1,24 @@
 import pytest
-from pathlib import Path
+
 from openplaces import cfg
-from openplaces.path import path, external_path  # adjust import
+from openplaces.path import external_path, path  # adjust import
 
 
 @pytest.mark.parametrize(
-    "call,expected",
+    'call,expected',
     [
-        (lambda: path(), lambda: cfg.dir_core / '_'),
-        (lambda: path(filename='data.csv'), lambda: cfg.dir_core / '_/data.csv'),
+        (lambda: path(), lambda: cfg.dir_core / '_all'),
+        (lambda: path(filename='data.csv'), lambda: cfg.dir_core / '_all/data.csv'),
         (
             lambda: path(
                 admin_id='US-MA-MI',
                 entity='parcel-massgis-2018',
                 dataset='water-lake-nhd-2022',
             ),
-            lambda: cfg.dir_core / 'US/MA/MI/_/parcel/massgis/2018/US-MA-MI_parcel-'
-            'massgis-2018_water-lake-nhd-2022.parquet',
+            lambda: (
+                cfg.dir_core / 'US/MA/MI/_all/parcel/massgis/2018/US-MA-MI_parcel-'
+                'massgis-2018_water-lake-nhd-2022.parquet'
+            ),
         ),
         (
             lambda: external_path(
@@ -24,8 +26,10 @@ from openplaces.path import path, external_path  # adjust import
                 dataset='water-lake-nhd-2019hr',
                 filename='NHD_H_North_Dakota_State_GDB.zip',
             ),
-            lambda: cfg.dir_external
-            / 'US/ND/_/water/lake/nhd/2019hr/NHD_H_North_Dakota_State_GDB.zip',
+            lambda: (
+                cfg.dir_external
+                / 'US/ND/_all/water/lake/nhd/2019hr/NHD_H_North_Dakota_State_GDB.zip'
+            ),
         ),
     ],
 )
